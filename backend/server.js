77ef4908a782711886120dd6f8776924c4c830c5
PrashanthKumarMadhasu <madhasuprashanth15@gmail.com>
2025-04-04 13:28:01 +0530

    Fixed embedded Git repo issue in recipe_finder

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+// server/server.js
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const filePath = "./favorites.json";
+
+const readFavorites = () => {
+  if (!fs.existsSync(filePath)) return [];
+  return JSON.parse(fs.readFileSync(filePath));
+};
+
+app.post("/favorites", (req, res) => {
+  const favorites = readFavorites();
+  const newFavorite = req.body;
+
+  if (!favorites.find(fav => fav.id === newFavorite.id)) {
+    favorites.push(newFavorite);
+    fs.writeFileSync(filePath, JSON.stringify(favorites, null, 2));
+  }
+  res.status(200).send("Added to favorites");
+});
+
+app.delete("/favorites/:id", (req, res) => {
+  let favorites = readFavorites();
+  const idToRemove = req.params.id;
+  favorites = favorites.filter(fav => fav.id !== idToRemove);
+  fs.writeFileSync(filePath, JSON.stringify(favorites, null, 2));
+  res.status(200).send("Removed from favorites");
+});
+
+app.get("/favorites", (req, res) => {
+    const favorites = readFavorites();
+    res.json(favorites);
+  });
+  
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
